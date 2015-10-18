44e6f536a244834a916cd02b4767a362be65877f
Niels Heirbaut <github@heirbaut.nl>
2015-10-18 05:28:52 +0200

    Add iterator set value unit test

--- tests/TestBoardValueAccess.cpp
+++ tests/TestBoardValueAccess.cpp
@@ -51,10 +51,33 @@ TEST_CASE("Can iterate over all board field", "[Board Access]")
     auto board = Sudoku::Board {blockSize};
 
     std::size_t numberOfFieldsOnBoard {0};
-    for (auto boardIterator = board.begin(); boardIterator != board.end(); ++boardIterator)
+    for (auto boardIterator = board.cbegin(); boardIterator != board.cend(); ++boardIterator)
     {
         ++numberOfFieldsOnBoard;
     }
 
     REQUIRE(numberOfFieldsOnBoard == blockSize * blockSize);
+}
+
+TEST_CASE("Can change board values using iterators", "[Board Access]")
+{
+    const Sudoku::Board::FieldType testValue {1};
+    const Sudoku::Board::FieldType blockSize {4};
+    auto board = Sudoku::Board {blockSize};
+
+    for (auto boardIterator = board.begin(); boardIterator != board.end(); ++boardIterator)
+    {
+        *boardIterator = testValue;
+    }
+
+    std::size_t numberOfCorrectlySetTestValues {0};
+    for (auto boardIterator = board.cbegin(); boardIterator != board.cend(); ++boardIterator)
+    {
+        if (*boardIterator == testValue)
+        {
+            ++numberOfCorrectlySetTestValues;
+        }
+    }
+
+    REQUIRE(numberOfCorrectlySetTestValues == blockSize * blockSize);
 }
