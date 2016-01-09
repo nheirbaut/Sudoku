a61b8d05833f85291065ea69913c3826d5bc05af
Niels Heirbaut <github@heirbaut.nl>
2016-01-09 12:05:30 +0100

    Add initial unit test for setting a Cells value

--- tests/TestCellValueAccess.cpp
+++ tests/TestCellValueAccess.cpp
@@ -1,7 +1,11 @@
-#include "TestCellValueAccess.h"
+#include <catch.hpp>
 
-TestCellValueAccess::TestCellValueAccess()
+#include "Cell.h"
+
+TEST_CASE("Cannot set a cell value larger than the set maximum", "[Cell Access]")
 {
+    const Sudoku::Cell::ValueType maximumCellValue {1};
+    Sudoku::Cell cell {maximumCellValue};
 
+    REQUIRE_THROWS_AS(cell = maximumCellValue + 1, std::out_of_range);
 }
-
