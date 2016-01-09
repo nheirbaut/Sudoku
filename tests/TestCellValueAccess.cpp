7fa303663ecddaaa8b00b5f6d647f21b5e9b06fc
Niels Heirbaut <github@heirbaut.nl>
2016-01-09 12:15:08 +0100

    Add unit test for implicit cell value conversion

--- tests/TestCellValueAccess.cpp
+++ tests/TestCellValueAccess.cpp
@@ -9,3 +9,15 @@ TEST_CASE("Cannot set a cell value larger than the set maximum", "[Cell Access]"
 
     REQUIRE_THROWS_AS(cell = maximumCellValue + 1, std::out_of_range);
 }
+
+TEST_CASE("Can convert a cell directly to its value", "[Cell Access]")
+{
+    const Sudoku::Cell::ValueType maximumCellValue {2};
+    const auto cellTestValue {maximumCellValue - 1};
+    Sudoku::Cell cell {maximumCellValue};
+
+    cell = cellTestValue;
+    const Sudoku::Cell::ValueType convertedCellValue {cell};
+
+    REQUIRE(convertedCellValue == cellTestValue);
+}
