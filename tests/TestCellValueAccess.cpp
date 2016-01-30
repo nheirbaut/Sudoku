4f9e4c12df680c1854b4730bc905d7c3c50682b0
Niels Heirbaut <github@heirbaut.nl>
2016-01-30 09:25:43 +0100

    Add test for cell initialization

--- tests/TestCellValueAccess.cpp
+++ tests/TestCellValueAccess.cpp
@@ -2,6 +2,14 @@
 
 #include "Cell.h"
 
+TEST_CASE("A cell is initialized with its invalid value", "[Cell Access]")
+{
+    const Sudoku::Cell::ValueType maximumCellValue {1};
+    Sudoku::Cell cell {maximumCellValue};
+
+    REQUIRE(cell == Sudoku::Cell::UNSET_CELL_VALUE);
+}
+
 TEST_CASE("Cannot set a cell value larger than the set maximum", "[Cell Access]")
 {
     const Sudoku::Cell::ValueType maximumCellValue {1};
